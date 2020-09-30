950440cb2930740066482b8250eea183ed0000fc
Tanner Waltz <49100554+TannerWaltz@users.noreply.github.com>
2020-09-30 15:26:21 -0400

    Added new columns for work positions and mental health conditions

--- cleaningdata.py
+++ cleaningdata.py
@@ -29,4 +29,41 @@ prodAffectPercentCat = pd.api.types.CategoricalDtype(categories=['1-25%', '26-50
 df['prodAffectPercent'] = df['prodAffectPercent'].astype(prodAffectPercentCat)
 
 ## possible ordered: leaveDifficulty
-## needs to be fixed: yesConditions, maybeConditions, professionalConditions, gender, workPosition
+## needs to be fixed: gender
+
+# Creating a Bool column if a person is each work position
+# Categories for work positions
+workPos = {'Back-end Developer':'backend', 'Front-end Developer':'frontend', 'Supervisor/Team Lead':'supervisor', 'DevOps/SysAdmin':'devOps', 'Other':'other', 'Support':'support', 'One-person shop':'onePersonShop', 'Designer':'designer', 'Dev Evangelist/Advocate':'advocate', 'Executive Leadership':'execLeader', 'Sales':'sales', 'HR':'hr'}
+
+# creates column
+for col in workPos.values():
+    df[col] = False
+
+for i, entry in enumerate(df['workPosition']): # for each person
+    for position in workPos.keys(): # for each possible position
+        if position in entry:
+            df.at[i, workPos[position]] = True
+        else:
+            df.at[i, workPos[position]] = False
+
+#Creating a bool column if a person has each condition for each method of diagnosis
+# Categories for mental conditions
+mentalCond = {'Mood Disorder (Depression, Bipolar Disorder, etc)':'mood', 'Anxiety Disorder (Generalized, Social, Phobia, etc)':'anxiety', 'Attention Deficit Hyperactivity Disorder':'adhd', 'Post-traumatic Stress Disorder':'ptsd', 'Obsessive-Compulsive Disorder':'ocd', 'Personality Disorder (Borderline, Antisocial, Paranoid, etc)':'personality', 'Stress Response Syndromes':'srs', 'Substance Use Disorder':'substance', 'Eating Disorder (Anorexia, Bulimia, etc)':'eating', 'Addictive Disorder':'addictive', 'Psychotic Disorder (Schizophrenia, Schizoaffective, etc)':'psychotic', 'Dissociative Disorder':'disassociative', 'Other':'other'}
+
+diagnoses = {'yesConditions':'Yes', 'maybeConditions':'Maybe', 'professionalConditions':'Profess'}
+
+# Creates columns
+for d in diagnoses.values():
+    for col in mentalCond.values():
+        df[col + d] = False
+
+for d in diagnoses.keys(): # For each type of diagnoses
+    for i, entry in enumerate(df[d]): # For each person
+        for condition in mentalCond.keys(): # For each condition
+            if entry != entry: # Checks if NaN (leaves it false)
+                continue
+            if condition in entry:
+                df.at[i, mentalCond[condition]+diagnoses[d]] = True
+            else:
+                df.at[i, mentalCond[condition]+diagnoses[d]] = False
+
