741df527d4b9f2eafd984c5817aca48abacf8a53
centipeda <joshua.c.guam@gmail.com>
2020-10-21 16:34:43 -0400

    change colors

--- dataviz.py
+++ dataviz.py
@@ -1,34 +1,51 @@
 import pandas as pd
-# import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.io as pio
+import plotly.graph_objects as go
 
+# load in the dataset
 df = pd.read_pickle('mhit.pd')
 gender = df.gender.value_counts()
 
-fig = px.bar(gender, y=["Male", "Female", "Gender\nminority"], x=gender.array, title="Responses by Gender", template="plotly_dark", orientation='h', labels={
-    "y": "Gender",
-    "x": "Responses"
-})
-fig.update_layout(hoverlabel={
-    "bgcolor": "black",
-    "bordercolor": "black",
-    "font": {
+# create custom template off of plotly_dark to match the site
+pio.templates["mhit"] = go.layout.Template(dict(layout=go.Layout(
+    hoverlabel={
+        "bgcolor": "black",
+        "bordercolor": "#1ae843",
+        "font": {
+            "family": "Source Code Pro",
+            "color": "#1ae843"
+        }
+    },
+    font={
         "family": "Source Code Pro",
         "color": "#1ae843"
+    },
+    paper_bgcolor="black",
+    plot_bgcolor="black",
+    margin={
+        "pad": 20
+    },
+    colorway=["#1ae843"]
+)))
+pio.templates.default = "plotly_dark+mhit"
+
+# create the bar graph
+fig = px.bar(gender, 
+    y=["Male", "Female", "Gender\nminority"], 
+    x=gender.array, 
+    title="Responses by Gender", 
+    orientation='h', 
+    labels={
+        "y": "Gender",
+        "x": "Responses"
     }
-},
-font={
-    "family": "Source Code Pro",
-    "color": "#1ae843"
-},
-paper_bgcolor="black",
-plot_bgcolor="black"
 )
-fig.update_traces(marker_color="#e8961a")
-# fig.show()
+
+# save the figure to an html doc
 fig.write_html("docs/plotly.html")
 
-# kinda hacky, but load the custom font into our generated HTML
+# kinda hacky, but load the custom font into the generated HTML
 with open("docs/plotly.html", "r+") as f:
     l = f.readlines()
     l[1] = '<head><meta charset="utf-8" /><link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,300;0,400;0,600;0,900;1,700&display=swap" rel="stylesheet"></head>'
