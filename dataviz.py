aced615b2da89cbb22e33374dad5cc2233a8b83b
centipeda <joshua.c.guam@gmail.com>
2020-10-22 17:13:17 -0400

    update cleaning data text, table

--- dataviz.py
+++ dataviz.py
@@ -41,7 +41,7 @@ fig = px.bar(gender,
         "x": "Responses"
     }
 )
-fig.write_html("docs/gender.html")
+fig.write_html("docs/figs/gender.html")
 
 fig = px.bar(df.nEmployees.value_counts().sort_index(),
     title="Responses by Company Size",
@@ -51,7 +51,7 @@ fig = px.bar(df.nEmployees.value_counts().sort_index(),
     }
 )
 fig.update_layout(showlegend=False)
-fig.write_html("docs/company_size.html")
+fig.write_html("docs/figs/company_size.html")
 
 import pca
 fig = px.scatter(x=pca.discussorX, y=pca.discussorY)
@@ -79,14 +79,14 @@ fig.update_layout(
     ),
     title="Principle Component Analysis Plot"
 )
-fig.write_html("docs/pca.html")
+fig.write_html("docs/figs/pca.html")
 
 aic = pd.read_csv('data/aic.csv')
 fig = px.bar(aic.sort_values(by="Slope"), 
     x="Question", 
     y="Slope", 
     title="Logistic Regression Model Weights")
-fig.write_html("docs/aic.html")
+fig.write_html("docs/figs/aic.html")
 
 pvals = pd.read_csv('data/logistic_regression.csv')
 fig = px.bar(pvals.sort_values(by='p-value'), 
@@ -94,7 +94,7 @@ fig = px.bar(pvals.sort_values(by='p-value'),
     log_y=True,
     title="P-values for predictor variables"
 )
-fig.write_html("docs/pval.html")
+fig.write_html("docs/figs/pval.html")
 
 train = pd.read_csv('data/best_calc.csv')
 fig = px.bar(train,
@@ -104,15 +104,19 @@ fig = px.bar(train,
     orientation="h"
 )
 fig.update_xaxes(nticks=10)
-fig.write_html("docs/training.html")
+fig.write_html("docs/figs/training.html")
 
 df = pd.read_csv("data/cleaned_mental-health-in-tech-2016.csv")
 short = df.loc[~df.selfEmployed].head(10)
 fig = go.Figure(data=go.Table(
     header=dict(
-        values=list(df.columns[1:]),
-        line_color="#1ae834",
-        fill_color="black"
+        values=[f"<b>{x}</b>" for x in df.columns[1:]],
+        fill_color="#1ae834",
+        line_color="black",
+        font=dict(
+            family="Source Code Pro",
+            color="black"
+        ),
     ),
     cells=dict(
         values=[short[x] for x in list(df.columns[1:])],
@@ -129,9 +133,9 @@ fig.update_layout(
         t=0
     )
 )
-fig.write_html("docs/table.html")
+fig.write_html("docs/figs/table.html")
 
-figs = ["docs/gender.html", "docs/company_size.html", "docs/pca.html", "docs/aic.html", "docs/pval.html", "docs/training.html", "docs/table.html"]
+figs = ["docs/figs/gender.html", "docs/figs/company_size.html", "docs/figs/pca.html", "docs/figs/aic.html", "docs/figs/pval.html", "docs/figs/training.html", "docs/figs/table.html"]
 
 # kinda hacky, but load the custom font into the generated HTML
 for figure in figs:
