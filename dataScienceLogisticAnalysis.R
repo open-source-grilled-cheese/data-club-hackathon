15f4b346fe6b71a04b4aa4c69faf44d1c7848ea4
Tanner Waltz <49100554+TannerWaltz@users.noreply.github.com>
2020-10-25 15:47:48 -0400

    Updated final R code

--- dataScienceLogisticAnalysis.R
+++ dataScienceLogisticAnalysis.R
@@ -1,6 +1,7 @@
 library(MASS)
 library(plyr)
 library(readr)
+library(caTools)
 
 # Reading in data from file
 newdata <- read_csv("data/cleaned_mental-health-in-tech-2016.csv")
@@ -8,8 +9,15 @@ newdata <- read_csv("data/cleaned_mental-health-in-tech-2016.csv")
 # Getting only rows of people who are not self-employed
 work = newdata[newdata$selfEmployed == F,]
 
-# Changing response variable to binary- combining No and Maybe (can change)
-work$discussSupervisor <- mapvalues(work$discussSupervisor, from=c("Yes", "No", "Maybe"), to=c(0, 1, 1))
+# Changing response variable to binary- combining No and Maybe
+work$discussSupervisor <- mapvalues(work$discussSupervisor, from=c("Yes", "No", "Maybe"), to=c(1, 0, 0))
+
+# Creating train and test datasets
+set.seed(18414) # set for consistency
+smp_siz = floor(0.80*nrow(work))
+train_ind = sample(seq_len(nrow(work)),size = smp_siz)
+train = work[train_ind,]
+test = work[-train_ind,]
 
 # Creating and viewing massive model
 model.regress = glm(as.factor(discussSupervisor)~as.factor(nEmployees) + techComp + as.factor(offerBenefits) + as.factor(formalDiscuss) + as.factor(offerResources) + as.factor(anonProtected) + as.factor(leaveDifficulty) + as.factor(discussMentalConseq) + as.factor(discussPhysicalConseq) + as.factor(discussCoworker) + as.factor(mentalVsPhysical) + as.factor(coworkerNegConseq) + as.factor(familyHistory) + as.factor(mentalPast) + as.factor(mentalCurrent) + as.factor(professionalDiagnose) + soughtTreatment + age + as.factor(gender) + as.factor(countryLive) + as.factor(workRemote) + backend + frontend + supervisor + devOps + other + support + onePersonShop + designer + advocate + execLeader + sales + hr, family="binomial", data = work)
@@ -44,7 +52,43 @@ pred = prediction(model.smaller$fitted.values, as.factor(work$discussSupervisor)
 perf = performance(pred, measure = "tpr", x.measure = "fpr")
 plot(perf, colorize=T)
 abline(a=1, b=-1)
+cutoff = 0.5
 
 # Area under curve
 auc.tmp = performance(pred, "auc")
 auc = as.numeric(auc.tmp@y.values) # Since over 80%, it is a very good model for classification
+
+# Prediction Code
+#################################################
+# Fitting smaller model with training data
+model.smaller = glm(as.factor(discussSupervisor)~techComp + as.factor(leaveDifficulty) + as.factor(discussMentalConseq) + as.factor(discussCoworker) + as.factor(mentalVsPhysical) + as.factor(familyHistory) + as.factor(workRemote) + supervisor + support + onePersonShop, family="binomial", data = train)
+
+# Seeing Predictions of train data
+results = model.smaller$fitted.values
+for (i in 1:(length(model.smaller$fitted.values))){
+  results[i] = (model.smaller$fitted.values[i] >= cutoff)
+}
+
+for (i in 1:(length(model.smaller$fitted.values))){
+  results[i] = (train$discussSupervisor[i] == results[i])
+}
+c = count(results)
+trainpercent = (c$freq[2]/(c$freq[1] + c$freq[2]))
+
+# Predicting test data
+testdf = data.frame(test$techComp, as.factor(test$leaveDifficulty), as.factor(test$discussMentalConseq), as.factor(test$discussCoworker), as.factor(test$mentalVsPhysical), as.factor(test$familyHistory), as.factor(test$workRemote), test$supervisor, test$support, test$onePersonShop, data = test)
+colnames(testdf) = c("techComp", "leaveDifficulty", "discussMentalConseq", "discussCoworker", "mentalVsPhysical", "familyHistory", "workRemote", "supervisor", "support", "onePersonShop")
+guesses = predict.glm(model.smaller, newdata = testdf)
+results = guesses
+
+for (i in 1:(length(guesses))){
+  results[i] = (guesses[i] >= cutoff)
+}
+
+for (i in 1:(length(guesses))){
+  results[i] = (test$discussSupervisor[i] == results[i])
+}
+c = count(results)
+testpercent = (c$freq[2]/(c$freq[1] + c$freq[2]))
+
+
